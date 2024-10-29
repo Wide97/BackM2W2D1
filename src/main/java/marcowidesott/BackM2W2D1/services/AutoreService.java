7c26b8bd41247e925d45bd8445d9583e006fbcad
Wide97 <mwidesott@gmail.com>
2024-10-29 15:23:39 +0100

    init services

---
+++ src/main/java/marcowidesott/BackM2W2D1/services/AutoreService.java
@@ -0,0 +1,12 @@
+package marcowidesott.BackM2W2D1.services;
+
+import marcowidesott.BackM2W2D1.entities.Autore;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AutoreService {
+    private List<Autore> autoreList = new ArrayList<>();
+}
---
+++ src/main/java/marcowidesott/BackM2W2D1/services/BlogService.java
@@ -0,0 +1,12 @@
+package marcowidesott.BackM2W2D1.services;
+
+import marcowidesott.BackM2W2D1.entities.Blog;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BlogService {
+    private List<Blog> listBlog = new ArrayList<>();
+}
